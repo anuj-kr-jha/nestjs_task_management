7d7b0f8a19f30261f88c6f192cfcd99d9786892f
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2022-07-10 08:18:05 +0530

    removed unused controller tasks2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
-import { Tasks2Controller } from './tasks2/tasks2.controller';
 
 @Module({
   imports: [TasksModule],
-  controllers: [Tasks2Controller],
 })
 export class AppModule {}
--- src/tasks2/tasks2.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('tasks2')
-export class Tasks2Controller {}
