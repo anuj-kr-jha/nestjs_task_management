e58bf6628fe06759d8414ca2a8bb3432d3617210
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2022-07-10 08:47:03 +0530

    dependency injection
    - inject service to controller
    - add get decorator to respond to get request

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks') // - this is responsible for the tasks/ endpoint
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {
+    // -tasksService will be added to this.taskService
+  }
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+}
