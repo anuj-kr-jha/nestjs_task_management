c23d8fe4ff30736feb1153a2a2510f4a5b720944
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2022-07-10 06:10:21 +0530

    adding new module
    -added tasks module

--- README.md
+++ README.md
@@ -14,6 +14,8 @@ create a new project using nest cli
 nest new task-management
 ```
 
+> it will generate boilerplate code for us to start with
+
 add fastify as well
 
 ```bash
@@ -21,3 +23,11 @@ yarn add @nestjs/platform-fastify
 ```
 
 > note: when using fastify their are some changes that we need to make. that we will cover later [ref](https://docs.nestjs.com/techniques/performance)
+
+## add a new module
+
+generate new module using nest cli
+
+```bash
+nest g module tasks
+```
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module'; // - AppModule is the root module and is the main module of the application
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule); // - for express , we dont need to pass adapter unlike fastify as it is used by default
   await app.listen(3000);
 }
 bootstrap();
